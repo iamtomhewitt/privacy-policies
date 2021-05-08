98a17e5af90eca3515b69cdb03a63ec6d7934503
Tom Hewitt <tom.hewitt@williamhill.com>
2021-05-08 13:29:03 +0100

    Updates

---
+++ precache-manifest.3342a94074267a529df82ad0b18a85c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad7b8f9288272c3ef65ba2c87ab3a32a",
+    "url": "/privacy-policies/index.html"
+  },
+  {
+    "revision": "caf70f44b24a9d16fea1",
+    "url": "/privacy-policies/static/css/main.59418a36.chunk.css"
+  },
+  {
+    "revision": "8fcbf7ff1832c547c871",
+    "url": "/privacy-policies/static/js/2.755a5bfa.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/privacy-policies/static/js/2.755a5bfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caf70f44b24a9d16fea1",
+    "url": "/privacy-policies/static/js/main.a26ed7d1.chunk.js"
+  },
+  {
+    "revision": "e3d1ce832081335c07c7",
+    "url": "/privacy-policies/static/js/runtime-main.7ab1133f.js"
+  }
+]);
